d84385e3f9fa4a16a1a06d0471b9172e0a930352
Rick Groot <rick.groot@wearetriple.com>
2022-06-02 15:31:34 +0200

    fix: quiz answer overlay refactor

--- source/ui/answer-popup/answerPopup.tsx
+++ source/ui/answer-popup/answerPopup.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { QuestionResult } from '~source/types/questions';
+import { ColoredBox } from '~source/ui';
 import cx from 'classnames';
 
 import $ from './answerPopup.module.scss';
@@ -7,26 +8,32 @@ import { getXp } from '~source/core/getResults';
 
 const AnswerPopup = ({ data }: { data: QuestionResult }) => {
     return (
-        <section className={$.container}>
-            <div
-                className={cx(
-                    $.message,
-                    data.isCorrect && $.messageCorrect,
-                    !data.isCorrect && $.messageIncorrect,
-                )}
+        <section
+            className={cx(
+                $.container,
+                data.isCorrect && $.containerCorrect,
+                !data.isCorrect && $.containerIncorrect,
+            )}
+        >
+            <h1 className={$.title}>
+                {data.isCorrect ? 'Nice one!' : 'Whoops, that is incorrect!'}
+            </h1>
+            <ColoredBox
+                className={cx($.message)}
+                color={data.isCorrect ? 'green' : 'red'}
+                animate
             >
-                <p className={$.text}>{data.isCorrect ? 'Nice!' : 'Whoops!'}</p>
                 <p className={$.icon}>{data.isCorrect ? '\u2713' : '\u2717'}</p>
-                <p className={$.description}>
-                    {data.isCorrect && `+${getXp([data])}xp`}
-                    {!data.isCorrect && 'Correct answer:'}
-                    {!data.isCorrect && (
-                        <span className={$.descriptionBold}>
-                            {data.correctAnswer}
-                        </span>
-                    )}
-                </p>
-            </div>
+            </ColoredBox>
+            <p className={$.description}>
+                {data.isCorrect && `+${getXp([data])}xp`}
+                {!data.isCorrect && 'Correct answer:'}
+                {!data.isCorrect && (
+                    <span className={$.descriptionBold}>
+                        {data.correctAnswer}
+                    </span>
+                )}
+            </p>
         </section>
     );
 };
