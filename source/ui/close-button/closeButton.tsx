bf8ba0a2f00050a6425f6feb9dfb7dffcafd78e1
Rick Groot <rick.groot@wearetriple.com>
2022-06-02 14:55:46 +0200

    fix: changes closebutton quiz component

--- source/ui/close-button/closeButton.tsx
+++ source/ui/close-button/closeButton.tsx
@@ -5,7 +5,6 @@ import cx from 'classnames';
 import $ from './closeButton.module.scss';
 
 const CloseButton = () => {
-    const [isOpen, setIsOpen] = React.useState<boolean>(false);
     const router = useRouter();
     const handleRedirect = () => {
         const { test } = router.query;
@@ -13,39 +12,12 @@ const CloseButton = () => {
         else router.push({ pathname: `/learn/${test}` });
     };
     return (
-        <section className={$.container}>
-            {!isOpen && (
-                <button
-                    type="button"
-                    className={cx($.button, $.buttonCross)}
-                    aria-label="quit quiz"
-                    onClick={() => setIsOpen(!isOpen)}
-                />
-            )}
-            {isOpen && (
-                <div className={$.chooseContainer}>
-                    <button
-                        type="button"
-                        className={cx($.button, $.buttonNo)}
-                        aria-label="quit quiz"
-                        onClick={() => setIsOpen(!isOpen)}
-                    >
-                        No
-                    </button>
-                    <button
-                        type="button"
-                        className={cx($.button, $.buttonYes)}
-                        aria-label="continue"
-                        onClick={() => handleRedirect()}
-                    >
-                        Yes
-                    </button>
-                    <p className={$.chooseContainerMessage}>
-                        Are you sure you want to quit?
-                    </p>
-                </div>
-            )}
-        </section>
+        <button
+            type="button"
+            className={cx($.button)}
+            aria-label="quit quiz"
+            onClick={() => handleRedirect()}
+        />
     );
 };
 
